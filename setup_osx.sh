fbc306267a9ba371c6a55ee6817ff7fe288cf498
Jippen <user@legitemail.xyz>
2017-08-22 16:45:42 -0700

    Add more stuff to osx setup

--- setup_osx.sh
+++ setup_osx.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-brew install fish
-brew install tldr
 
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install things via homebrew
+brew install wget tree curl tldr fish
+
+# Setup python environment
+pip install -r pip.txt
